28ef3358079b3e807ed3f8103c096ce83606a970
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2017-05-04 19:57:12 +0300

    [chore] added jest config

---
+++ src/components/__tests__/Footer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from '../Footer/Footer';
+
+test('<Footer />', () => {
+  const footer = shallow(
+    <Footer />
+  );
+
+  expect(footer.find('div').length).toBe(1);
+});
