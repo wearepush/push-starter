4286c11cfb8f7fd232ebd9f157b2f5dda14c602e
Misha <mikhail.karpov.s@gmail.com>
2017-06-01 13:36:32 +0300

    [fix] origin app

--- src/app/containers/App/App.js
+++ src/app/containers/App/App.js
@@ -3,39 +3,30 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import { asyncConnect } from 'redux-connect';
 import Helmet from 'react-helmet';
-import { func, object } from 'prop-types';
-import config from './config';
-
+import { object, func } from 'prop-types';
 import { isLoaded as isAuthLoaded, load as loadAuth } from '../../redux/modules/auth';
+import { Header, Footer } from '../../components';
+import config from './config';
 import styles from './App.scss';
 
-@asyncConnect([{
-  promise: ({ store: { dispatch, getState } }) => {
-    const promises = [];
-    const state = getState();
-    if (!state.reduxAsyncConnect.loaded && !isAuthLoaded(state) && !state.auth.error) {
-      promises.push(dispatch(loadAuth()));
-    }
-    return Promise.all(promises);
-  }
-}])
-@connect(
-  state => ({
-    user: state.auth.user
-  }),
-  {
-    pushState: push
-  }
-)
-export default class App extends Component {
+const mapStateToProps = st => ({
+  user: st.auth.user
+});
+const mapDispatchToProps = {
+  pushState: push
+};
+
+class App extends Component {
   static propTypes = {
     children: object,
-    user: object,
-    pushState: func.isRequired
+    // history: object, // eslint-disable-line
+    pushState: func.isRequired,
+    user: object
   };
 
   static defaultProps = {
     children: null,
+    // history: null,
     user: null
   };
 
@@ -53,8 +44,21 @@ export default class App extends Component {
     return (
       <div className={styles.app}>
         <Helmet {...config.meta.head} />
+        <Header />
         <div className={styles.app__container}>{this.props.children}</div>
+        <Footer />
       </div>
     );
   }
 }
+
+export default asyncConnect([{
+  promise: ({ store: { dispatch, getState } }) => {
+    const promises = [];
+    const state = getState();
+    if (!state.reduxAsyncConnect.loaded && !isAuthLoaded(state) && !state.auth.error) {
+      promises.push(dispatch(loadAuth()));
+    }
+    return Promise.all(promises);
+  }
+}])(connect(mapStateToProps, mapDispatchToProps)(App));
