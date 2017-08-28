9158889d764fa3890c659937e329dcfaf56c7f51
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2017-08-28 19:34:33 +0300

    [chore] add webpack analyzer

--- webpack/webpack.config.client.production.babel.js
+++ webpack/webpack.config.client.production.babel.js
@@ -97,7 +97,7 @@ const config = {
       // See "Definitions" section for more information.
       defaultSizes: 'parsed',
       // Automatically open report in default browser
-      openAnalyzer: true,
+      openAnalyzer: false,
       // If `true`, Webpack Stats JSON file will be generated in bundles output directory
       generateStatsFile: false,
       // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
