37e3e7f200a8623331e2ef34dd8874ac36dce459
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2018-05-12 22:02:06 +0300

    [feat] #51 add textfield test and button component

--- src/elements/Button/Button.js
+++ src/elements/Button/Button.js
@@ -1,25 +1,187 @@
-import React from 'react';
-import { string, node } from 'prop-types';
+import React, { PureComponent } from 'react';
+import { bool, func, node, number, oneOfType, string } from 'prop-types';
+import cx from 'classnames';
+import styles from './Button.scss';
 
-const Button = ({
-  children,
-  ...rest
-}) => (
-  <button
-    {...rest}
-  >
-    {children}
-  </button>
-);
+export default class Button extends PureComponent {
+  static propTypes = {
+    /**
+    * Use that property to pass a ref callback to the native button component.
+    */
+    buttonRef: func,
+    /**
+    * The content of the button.
+    */
+    children: node.isRequired,
+    /**
+     * @ignore
+     */
+    className: string,
+    /**
+    * The component used for the root node.
+    * Either a string to use a DOM element or a component.
+    * The default value is a `button`.
+    */
+    component: oneOfType([
+      string,
+      func
+    ]),
+    /**
+    * If `true`, the base button will be disabled.
+    */
+    disabled: bool,
+    /**
+    * @ignore
+    */
+    onBlur: func,
+    /**
+    * @ignore
+    */
+    onClick: func,
+    /**
+    * @ignore
+    */
+    onFocus: func,
+    /**
+    * @ignore
+    */
+    onKeyDown: func,
+    /**
+    * @ignore
+    */
+    onKeyUp: func,
+    /**
+    * @ignore
+    */
+    onMouseDown: func,
+    /**
+    * @ignore
+    */
+    onMouseLeave: func,
+    /**
+    * @ignore
+    */
+    onMouseUp: func,
+    /**
+    * @ignore
+    */
+    onTouchEnd: func,
+    /**
+    * @ignore
+    */
+    onTouchMove: func,
+    /**
+    * @ignore
+    */
+    onTouchStart: func,
+    /**
+    * @ignore
+    */
+    role: string,
+    /**
+    * @ignore
+    */
+    tabIndex: oneOfType([
+      number,
+      string
+    ]),
+    /**
+    * @ignore
+    */
+    type: string,
+  };
 
-Button.propTypes = {
-  type: string,
-  children: node
-};
+  static defaultProps = {
+    buttonRef: undefined,
+    className: '',
+    component: 'button',
+    disabled: false,
+    onBlur: undefined,
+    onClick: undefined,
+    onFocus: undefined,
+    onKeyDown: undefined,
+    onKeyUp: undefined,
+    onMouseDown: undefined,
+    onMouseLeave: undefined,
+    onMouseUp: undefined,
+    onTouchEnd: undefined,
+    onTouchMove: undefined,
+    onTouchStart: undefined,
+    role: 'button',
+    tabIndex: 0,
+    type: 'button',
+  };
 
-Button.defaultProps = {
-  type: 'button',
-  children: null
-};
+  render() {
+    const {
+      buttonRef,
+      children,
+      className: classNameProp,
+      component,
+      disabled,
+      onClick,
+      onBlur,
+      onFocus,
+      onKeyDown,
+      onKeyUp,
+      onMouseDown,
+      onMouseLeave,
+      onMouseUp,
+      onTouchEnd,
+      onTouchMove,
+      onTouchStart,
+      role,
+      tabIndex,
+      type,
+      ...other
+    } = this.props;
 
-export default Button;
+    let ComponentProp = component;
+
+    if (!ComponentProp) {
+      if (other.href) {
+        ComponentProp = 'a';
+      } else {
+        ComponentProp = 'button';
+      }
+    }
+
+    const buttonProps = {};
+
+    if (ComponentProp === 'button') {
+      buttonProps.type = type || 'button';
+      buttonProps.disabled = disabled;
+    } else {
+      buttonProps.role = 'button';
+    }
+
+    const className = cx(styles.Button, {
+      [styles[classNameProp]]: !!classNameProp,
+      'is-disabled': disabled,
+    });
+
+    return (
+      <ComponentProp
+        className={className}
+        onClick={onClick}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onMouseDown={onMouseDown}
+        onMouseLeave={onMouseLeave}
+        onMouseUp={onMouseUp}
+        onTouchEnd={onTouchEnd}
+        onTouchMove={onTouchMove}
+        onTouchStart={onTouchStart}
+        ref={buttonRef}
+        role={role}
+        tabIndex={disabled ? '-1' : tabIndex}
+        {...buttonProps}
+        {...other}
+      >
+        {children}
+      </ComponentProp>
+    );
+  }
+}
---
+++ src/elements/FormTextField/__test__/FormTextField.test.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { reduxForm } from 'redux-form/immutable';
+import { noop } from 'lodash';
+import formStore from '../../../redux/__mocks__/formStore.js';
+import { TextField, FormField, FormTextField } from '../../index.js';
+
+let store;
+beforeEach(() => {
+  store = formStore;
+});
+
+const makeForm = ({
+  renderSpy = noop,
+  onFocusSpy = noop,
+  onChangeSpy = noop,
+  onBlurSpy = noop,
+}) => (
+  class Form extends Component {
+    render() {
+      renderSpy(this.props);
+      return (
+        <form>
+          <FormTextField
+            label="Title"
+            name="title"
+            placeholder="Title"
+            onFocus={onFocusSpy}
+            onChange={onChangeSpy}
+            onBlur={onBlurSpy}
+            value="0"
+          />
+        </form>
+      );
+    }
+  }
+);
+
+const renderForm = (Form, formState, config = {}) => {
+  const Decorated = reduxForm({ form: 'testForm', ...config })(Form);
+  return mount(
+    <Provider store={store}>
+      <Decorated />
+    </Provider>
+  );
+};
+
+describe('FormTextField', () => {
+  it('should render textfield with initial state', () => {
+    const renderSpy = jest.fn(() => {});
+    const Form = makeForm({ renderSpy });
+    const dom = renderForm(Form, {}, {});
+
+    expect(renderSpy).toHaveBeenCalled();
+    expect(renderSpy).toHaveBeenCalledTimes(1);
+
+    expect(dom.find(TextField).length).toBe(1);
+    expect(dom.find(FormField).length).toBe(1);
+
+    dom.unmount();
+  });
+
+  it('should handle onFocus, onChange, onBlur', () => {
+    const renderSpy = jest.fn(() => {});
+    const onFocusSpy = jest.fn();
+    const onChangeSpy = jest.fn();
+    const onBlurSpy = jest.fn();
+    const Form = makeForm({
+      renderSpy,
+      onFocusSpy,
+      onChangeSpy,
+      onBlurSpy
+    });
+    const dom = renderForm(Form, {}, {});
+
+    const inputElement = dom.find('.TextField__input');
+
+    // onFocus
+    inputElement.simulate('focus');
+
+    expect(onFocusSpy).toHaveBeenCalled();
+    expect(renderSpy).toHaveBeenCalledTimes(1);
+
+    // // onChange
+    inputElement.simulate('change', { target: { value: '0' } });
+    expect(onChangeSpy).toHaveBeenCalled();
+    expect(renderSpy).toHaveBeenCalledTimes(1);
+
+    // onBlur
+    inputElement.simulate('blur');
+    expect(onBlurSpy).toHaveBeenCalled();
+    expect(renderSpy).toHaveBeenCalledTimes(2);
+
+    dom.unmount();
+  });
+});
