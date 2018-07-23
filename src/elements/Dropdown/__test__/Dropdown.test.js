dbbb36a1a0e7f4c8ac4113a838d7d9365502456b
push-9 <dima.s@wearepush.co>
2018-07-23 19:58:25 +0300

    #51 [fix] tests

--- src/elements/Dropdown/__test__/Dropdown.test.js
+++ src/elements/Dropdown/__test__/Dropdown.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable prefer-destructuring */
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Dropdown from '../Dropdown.js';
@@ -17,13 +16,18 @@ describe('Dropdown', () => {
       );
       expect(dropdown.find('.Dropdown__button').length).toEqual(1);
       expect(dropdown.find(Button).length).toEqual(1);
-      expect(dropdown.prop('dropPosition')).toEqual(undefined);
-      expect(dropdown.prop('isOpen')).toEqual(undefined);
-      expect(dropdown.prop('buttonClassName')).toEqual(undefined);
+      const props = dropdown.instance().props;
+      expect(props.isOpen).toEqual(undefined);
+      expect(props.className).toBe('');
+      expect(props.classNameDefaultButton).toBe('');
+      expect(props.dropPosition).toBe('bl');
+      expect(props.isSelfClosed).toEqual(false);
+      expect(props.tabIndex).toBe(null);
+      expect(props.trigger).toBe('click');
     });
   });
 
-  describe('render dropdown menu', () => {
+  describe('uncontroled', () => {
     it('should render dropdown menu after click', () => {
       const dropdown = mount(
         <Dropdown
@@ -38,15 +42,13 @@ describe('Dropdown', () => {
       expect(dropdown.find('.dropdown__menu').length).toEqual(1);
       dropdown.unmount();
     });
-  });
 
-  describe('render dropdown menu with unique classNames', () => {
     it('should set up unique classes', () => {
       const dropdown = mount(
         <Dropdown
-          className='drop'
-          dropMenuClassName='my-menu-classname'
-          buttonClassName='my-button-classname'
+          className="drop"
+          dropMenuClassName="my-menu-classname"
+          buttonClassName="my-button-classname"
           button={<Button />}
         >
           {
@@ -60,9 +62,104 @@ describe('Dropdown', () => {
       expect(dropdown.hasClass('drop'));
       dropdown.unmount();
     });
+
+    it('should close dropdown menu when clicking outside component', () => {
+      const dropdown = mount(
+        <Dropdown
+          button={<Button />}
+        >
+          {
+            menuList.map((el, i) => <span key={i.toString()}>{el}</span>)
+          }
+        </Dropdown>
+      );
+
+      dropdown.find(Button).simulate('click');
+      expect(dropdown.find('.dropdown__menu').length).toEqual(1);
+      document.body.addEventListener('click', () => { });
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent('click', false, true);
+      document.body.dispatchEvent(evt);
+      dropdown.update();
+      expect(dropdown.find('.dropdown__menu').length).toEqual(0);
+      dropdown.unmount();
+    });
+
+    it('should close dropdown menu when clicking on list element', () => {
+      const dropdown = mount(
+        <Dropdown
+          isSelfClosed
+          button={<Button />}
+        >
+          {
+            menuList.map((el, i) => <span className="menu-item" key={i.toString()}>{el}</span>)
+          }
+        </Dropdown>
+      );
+      dropdown.find(Button).simulate('click');
+      expect(dropdown.find('.dropdown__menu').length).toEqual(1);
+      const listElem = dropdown.find('.menu-item').last();
+      listElem.simulate('click');
+      dropdown.update();
+      expect(dropdown.find('.dropdown__menu').length).toEqual(0);
+      dropdown.unmount();
+    });
+    it('should render default button when setup "string" in button prop', () => {
+      const dropdown = mount(
+        <Dropdown
+          button="btn"
+        >
+          {
+            menuList.map((el, i) => <span className="menu-item" key={i.toString()}>{el}</span>)
+          }
+        </Dropdown>
+      );
+      expect(dropdown.find('div.Dropdown__default_button').length).toEqual(1);
+      dropdown.unmount();
+    });
+
+
+    it('should open onMouseEnter and close after onMouseLeave', () => {
+      const dropdown = mount(
+        <Dropdown
+          button="btn"
+          trigger="hover"
+        >
+          {
+            menuList.map((el, i) => <span className="menu-item" key={i.toString()}>{el}</span>)
+          }
+        </Dropdown>
+      );
+
+      const btn = dropdown.find('.Dropdown__button');
+      btn.simulate('mouseenter');
+      dropdown.update();
+      expect(dropdown.find('.dropdown__menu').length).toEqual(1);
+      dropdown.simulate('mouseleave');
+      dropdown.update();
+      expect(dropdown.find('.dropdown__menu').length).toEqual(0);
+      dropdown.unmount();
+    });
   });
 
-  describe('dropdown menu is open', () => {
+  describe('controled', () => {
+    it('should be controlled component if parent do setState', () => {
+      const dropdown = shallow(
+        <Dropdown
+          button={<Button />}
+        >
+          {
+            menuList.map((el, i) => <span className="menu-item" key={i.toString()}>{el}</span>)
+          }
+        </Dropdown>
+      );
+
+      dropdown.setProps({ test: true });
+      dropdown.update();
+      expect(dropdown.state().isOpen).toBe(undefined);
+      dropdown.unmount();
+    });
+
     it('should render dropdown menu when component have isOpen prop', () => {
       const dropdown = mount(
         <Dropdown
@@ -77,93 +174,5 @@ describe('Dropdown', () => {
       expect(dropdown.find('.dropdown__menu').length).toEqual(1);
       dropdown.unmount();
     });
-
   });
-
-  describe('close dropdown menu', () => {
-    it('should close dropdown menu when clicking outside component', () => {
-
-      const dropdown = mount(
-        <Dropdown
-          button={<Button />}
-        >
-          {
-            menuList.map((el, i) => <span key={i.toString()}>{el}</span>)
-          }
-        </Dropdown>
-      );
-
-      dropdown.find(Button).simulate('click');
-      expect(dropdown.find('.dropdown__menu').length).toEqual(1);
-
-      document.body.addEventListener('click', () => { });
-      var evt = document.createEvent("HTMLEvents");
-      evt.initEvent("click", false, true);
-      document.body.dispatchEvent(evt)
-
-      dropdown.update();
-      expect(dropdown.find('.dropdown__menu').length).toEqual(0);
-      dropdown.unmount();
-    });
-  });
-
-  describe('close dropdown menu selfclosed component', () => {
-    it('should close dropdown menu when clicking on list element', () => {
-      const dropdown = mount(
-        <Dropdown
-          isSelfClosed
-          button={<Button />}
-        >
-          {
-            menuList.map((el, i) => <span className='menu-item' key={i.toString()}>{el}</span>)
-          }
-        </Dropdown>
-      );
-      dropdown.find(Button).simulate('click');
-      expect(dropdown.find('.dropdown__menu').length).toEqual(1);
-      const listElem = dropdown.find('.menu-item').last();
-      listElem.simulate('click');
-      dropdown.update();
-      expect(dropdown.find('.dropdown__menu').length).toEqual(0);
-      dropdown.unmount();
-    });
-  });
-
-  describe('render default button', () => {
-    it('should render default button when setup "string" in button prop', () => {
-      const dropdown = mount(
-        <Dropdown
-          button='btn'
-        >
-          {
-            menuList.map((el, i) => <span className='menu-item' key={i.toString()}>{el}</span>)
-          }
-        </Dropdown>
-      );
-      expect(dropdown.find('div.Dropdown__default_button').length).toEqual(1);
-      dropdown.unmount();
-    });
-  });
-
-
-  describe('should be controlled component', () => {
-    it('should be controlled component if parent do setState', () => {
-      const dropdown = shallow(
-        <Dropdown
-          button='btn'
-          button={<Button />}
-        >
-          {
-            menuList.map((el, i) => <span className='menu-item' key={i.toString()}>{el}</span>)
-          }
-        </Dropdown>
-      );
-      
-      dropdown.setProps({test: true});
-      dropdown.update();
-      expect(dropdown.state().isOpen).toBe(undefined);
-      dropdown.unmount();
-    });
-  });
-
 });
