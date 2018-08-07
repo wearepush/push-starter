c4cb67823dcf429038226c423b0353b3091d1c71
irmairma <irma@wearepush.co>
2018-08-07 11:28:52 +0300

    create form Reset Passowrd

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,4 @@ export Header from './Header/Header';
 export Footer from './Footer/Footer';
 export SignIn from './SignIn/SignIn';
 export SignUp from './SignUp/SignUp';
-// export ResetPassword from './ResetPassword/ResetPassword';
+export ResetPassword from './ResetPassword/ResetPassword';
