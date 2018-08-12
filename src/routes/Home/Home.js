b53b68b679737b766f3079cb409668987bf9a796
Irma Romanenko <irmairma@users.noreply.github.com>
2018-08-12 18:39:01 +0300

    create new pages (#102)

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
+
 const mapStateToProps = () => ({});
 
 const mapDispatchToProps = {};
