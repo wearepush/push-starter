9c0129682868496172e2b723ff6b107e7b03f5aa
TimurZ <timurztv@gmail.com>
2021-04-09 19:14:40 +0300

    feat(config): adds ScrollToTop

---
+++ src/elements/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,13 @@
+// https://reactrouter.com/web/guides/scroll-restoration
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
