c03f493aecc0b8e8363c83f350a4c219f616c4a2
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2021-04-14 20:01:16 +0300

    fix(heroku): fix heroku

--- src/server/server.js
+++ src/server/server.js
@@ -55,8 +55,9 @@ export default function (parameters) {
   }
 
   app.get('*', createSSR(parameters && parameters.chunks()));
-  console.log('port!!!!!!!!!!!! ', port);
-  const server = app.listen(port, (err) => {
+  console.log('port!!!!!!!!!!!! 2', port);
+  console.log('port!!!!!!!!!!!! 1', process.env.PORT);
+  const server = app.listen(process.env.PORT, (err) => {
     if (err) {
       return console.error(err);
     }
