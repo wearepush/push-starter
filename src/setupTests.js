b7d8745428114e274be909aae9be788a3ef2e5ea
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2021-06-02 13:11:08 +0300

    feat: fix enzyme adapter

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 import '../config/consts';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
